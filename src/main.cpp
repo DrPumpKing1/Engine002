2e579f9441e5ff74f4b1ea475f41685bbc02e2f5
Salvador Rodriguez <salvadortokumura@gmail.com>
2026-07-25 12:01:21 -0500

    feat (Neovim setup):
    Added cmake presets
    Added clang format with Google style

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+#include <stb/stb_image.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <stb/stb_image.h>
-
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-
-#include <iostream>
 #include <filesystem>
+#include <iostream>
 
 #include "Engine.h"
 
-const unsigned int WIDTH {800};
-const unsigned int HEIGHT {600};
+const unsigned int WIDTH{800};
+const unsigned int HEIGHT{600};
 unsigned int currentWidth{WIDTH};
 unsigned int currentHeight{HEIGHT};
 
@@ -29,66 +28,64 @@ float lastTime{0.0f};
 float lastCursorX{WIDTH / 2.0f};
 float lastCursorY{HEIGHT / 2.0f};
 bool firstCursorClick{true};
-bool cursorInGame {false};
+bool cursorInGame{false};
 
-int main(void)
-{
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+int main(void) {
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(currentWidth, currentHeight, "Hello World", nullptr, nullptr);
-    if (!window)
-    {
-        std::cerr << "Failed to create GLFW Window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-
-    if(!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glFrontFace(GL_CCW);
-
-    glEnable(GL_DEPTH_TEST);
-
-    glClearColor(0.8f, 0.6f, 0.4f, 1.0f);
-
-    while (!glfwWindowShouldClose(window))
-    {
-        float currentTime = static_cast<float>(glfwGetTime());
-        deltaTime = currentTime - lastTime;
-        float fps = 1.0f / deltaTime;
-        lastTime = currentTime;
-
-        glfwSetWindowTitle(window, ("Basic OpenGL Engine - FPS: " + std::to_string(fps)).c_str());
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-    }
-
+  GLFWwindow* window = glfwCreateWindow(currentWidth, currentHeight,
+                                        "Hello World", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create GLFW Window" << std::endl;
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+  if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+  glFrontFace(GL_CCW);
+
+  glEnable(GL_DEPTH_TEST);
+
+  glClearColor(0.8f, 0.6f, 0.4f, 1.0f);
+  while (!glfwWindowShouldClose(window)) {
+    float currentTime = static_cast<float>(glfwGetTime());
+    deltaTime = currentTime - lastTime;
+    float fps = 1.0f / deltaTime;
+    lastTime = currentTime;
+
+    glfwSetWindowTitle(
+        window, ("Basic OpenGL Engine - FPS: " + std::to_string(fps)).c_str());
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
 
-void FramebufferSizeCallback(GLFWwindow *window, int width, int height) {
-    currentWidth = width;
-    currentHeight = height;
-    glViewport(0, 0, width, height);
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+  currentWidth = width;
+  currentHeight = height;
+  glViewport(0, 0, width, height);
 }
